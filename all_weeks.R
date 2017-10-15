f4cbe9f8dc8bc4b379185c5164a21c45697af3bd
Josue Rodriguez <josue.rodriguez594@gmail.com>
2017-10-15 11:55:32 -0700

    all_weeks + power ranking workbook

---
+++ all_weeks.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(readxl)
+
+path <- '~/Documents/GitHub/power-rankings/power-rankings.xlsx'
+
+import_rankings <- map(excel_sheets(path),
+                         read_excel,
+                         path = path)
+
+power_rankings <- map(import_rankings, gather, key = ranker, value = team, -ranking)
+
+df_names <- paste('Week', 1:length(power_rankings), sep = " ") 
+names(power_rankings) <- df_names
+
+list2env(power_rankings, .GlobalEnv)
+
+all_weeks <- cbind(week1[3], week2[3], week3[3], week4[3], week5[3]) 
+colnames(all_weeks) <- df_names
+
+all_weeks <- cbind(week1[1], all_weeks )
+
+#all_weeks <- unclass(all_weeks) %>% as.data.frame()
+
+all_gathered <- na.omit(all_weeks) %>% gather(key = week, value = team, -ranking)
+all_gathered$week <- as.factor(all_gathered$week)
+all_gathered$team <- as.factor(all_gathered$team)
+
+ggplot(all_gathered, aes(x = week, y = ranking, group = team, color = team)) +
+  stat_summary(fun.y = mean, geom = 'line') +
+  labs(main = 'r/NFL Power Rankings', x = 'Weeks', y = 'Average Ranking') +
+  scale_y_continuous(trans = 'reverse')
+
+
+
+#####
+  # path <- "urbanpop.xls"
+  # urban_sheet1 <- read.xls(path, sheet = 1, stringsAsFactors = FALSE)
+  # urban_sheet2 <- read.xls(path, sheet = 2, stringsAsFactors = FALSE)
+  # urban_sheet3 <- read.xls(path, sheet = 3 , stringsAsFactors = FALSE)
+  # 
+  #   # Extend the cbind() call to include urban_sheet3: urban
+  # urban <- cbind(urban_sheet1, urban_sheet2[-1], urban_sheet3[-1])
+  
